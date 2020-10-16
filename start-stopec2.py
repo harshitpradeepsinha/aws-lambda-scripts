29c957bb3a3fd22970229b5597002fc5c4a849d9
harshitpradeepsinha <55779677+harshitpradeepsinha@users.noreply.github.com>
2020-10-16 17:22:34 +0530

    Create start-stopec2.py

---
+++ start-stopec2.py
@@ -0,0 +1,8 @@
+
+import boto3
+region = 'ec2-region'
+instances = ['ec2id','moreids']
+ec2 = boto3.client('ec2', region_name=region)
+def lambda_handler(event, context):
+    ec2.stop_instances(InstanceIds=instances) //(change start stop as you like)
+    print('stopped your instances: ' + str(instances))
